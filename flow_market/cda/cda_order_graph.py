189f10488fc185ed5e1de5d303086373dc0ad242
Zhangxi-Lam <jayce.linzx@gmail.com>
2022-07-04 17:51:59 -0700

    Combine order graph

--- flow_market/cda/cda_order_graph.py
+++ flow_market/cda/cda_order_graph.py
@@ -1,58 +1,50 @@
 from .cda_point import CdaPoint
 from .cda_order import CdaOrder
+from flow_market.common.order_graph import BaseOrderGraph
 from flow_market.common.constants import Y_MAX
 
 
-class CdaOrderGraph:
+class CdaOrderGraph(BaseOrderGraph):
     def __init__(self) -> None:
-        self.send_update_to_frontend = True
-
-        self.bid_active = True
         self.bid = CdaPoint(1, 1)
-        self.buy_order_id = None
-
-        self.ask_active = True
         self.ask = CdaPoint(2, 19)
-        self.sell_order_id = None
+        BaseOrderGraph.__init__(self)
 
-    def get_frontend_response(self):
-        if not self.send_update_to_frontend:
-            return
-        self.send_update_to_frontend = False
+    def has_data(self, is_bid):
+        if is_bid:
+            return self.bid is not None
+        return self.ask is not None
 
-        response = {
-            "bid_active": self.bid_active,
-            "buy_order_id": self.buy_order_id,
-            "ask_active": self.ask_active,
-            "sell_order_id": self.sell_order_id,
-        }
-        if self.bid:
-            response["bid_data"] = [
+    def get_data(self, is_bid):
+        if is_bid:
+            return [
                 {"x": 0, "y": self.bid.y},
                 {"x": self.bid.x, "y": self.bid.y},
                 {"x": self.bid.x, "y": 0},
             ]
-        if self.ask:
-            response["ask_data"] = [
-                {"x": 0, "y": self.ask.y},
-                {"x": self.ask.x, "y": self.ask.y},
-                {"x": self.ask.x, "y": 0},
-            ]
-        return response
+        return [
+            {"x": 0, "y": self.ask.y},
+            {"x": self.ask.x, "y": self.ask.y},
+            {"x": self.ask.x, "y": 0},
+        ]
 
     def add_order(self, order: CdaOrder):
-        self.send_update_to_frontend = True
-
-        is_buy = order.direction == "buy"
-        if is_buy:
-            self.bid_active = False
+        if order.direction == "buy":
             self.bid = order.point
-            self.buy_order_id = order.order_id
         else:
-            self.ask_active = False
             self.ask = order.point
-            self.sell_order_id = order.order_id
-        self.sort_points(adjust_buy=not is_buy)
+        BaseOrderGraph.add_order(self, order)
+
+    def remove_order(self, order: CdaOrder):
+        if order.direction == "buy":
+            self.bid = CdaPoint(1, 1)
+            if not self.ask:
+                self.ask = CdaPoint(2, 19)
+        else:
+            self.ask = CdaPoint(2, 19)
+            if not self.bid:
+                self.bid = CdaPoint(1, 1)
+        BaseOrderGraph.remove_order(self, order)
 
     def sort_points(self, adjust_buy):
         if self.ask.y <= self.bid.y:
@@ -60,40 +52,11 @@ class CdaOrderGraph:
                 if self.ask.y >= 2:
                     self.bid.y = self.ask.y - 1
                 else:
-                    self.remove_points(remove_buy=True)
+                    self.bid_active = False
+                    self.bid = None
             else:
                 if self.bid.y <= Y_MAX - 2:
                     self.ask.y = self.bid.y + 1
                 else:
-                    self.remove_points(remove_buy=False)
-
-    def remove_points(self, remove_buy):
-        if remove_buy:
-            self.bid_active = True
-            self.bid = None
-        else:
-            self.ask_active = True
-            self.ask = None
-
-    def remove_order(self, order: CdaOrder):
-        self.send_update_to_frontend = True
-
-        is_buy = order.direction == "buy"
-        if is_buy:
-            self.buy_order_id = None
-        else:
-            self.sell_order_id = None
-        self.reset_points(is_buy)
-
-    def reset_points(self, is_buy):
-        if is_buy:
-            self.bid_active = True
-            self.bid = CdaPoint(1, 1)
-            if not self.ask:
-                self.ask = CdaPoint(2, 19)
-        else:
-            self.ask_active = True
-            self.ask = CdaPoint(2, 19)
-            if not self.bid:
-                self.bid_active = True
-                self.bid = CdaPoint(1, 1)
+                    self.ask_active = False
+                    self.ask = None
--- flow_market/flo/flo_order_graph.py
+++ flow_market/flo/flo_order_graph.py
@@ -1,70 +1,60 @@
 from .flo_order import FloOrder
 from .flo_point import FloPoint
+from flow_market.common.order_graph import BaseOrderGraph
 from flow_market.common.constants import Y_MAX
 
 
-class FloOrderGraph:
-    def __init__(self) -> None:
-        self.send_update_to_frontend = True
-
-        self.bid_active = True
+class FloOrderGraph(BaseOrderGraph):
+    def __init__(self):
         self.bid_max = FloPoint(0, 1)
         self.bid_min = FloPoint(1, 0)
-        self.buy_order_id = None
-
-        self.ask_active = True
         self.ask_max = FloPoint(1, 20)
         self.ask_min = FloPoint(0, 19)
-        self.sell_order_id = None
+        BaseOrderGraph.__init__(self)
 
-    def get_frontend_response(self):
-        if not self.send_update_to_frontend:
-            return
-        self.send_update_to_frontend = False
+    def has_data(self, is_bid):
+        if is_bid:
+            return self.bid_max is not None
+        return self.ask_max is not None
 
-        response = {
-            "bid_active": self.bid_active,
-            "buy_order_id": self.buy_order_id,
-            "ask_active": self.ask_active,
-            "sell_order_id": self.sell_order_id,
-        }
-        if self.bid_max:
-            response["bid_data"] = [
+    def get_data(self, is_bid):
+        if is_bid:
+            return [
                 {"x": self.bid_max.x, "y": Y_MAX},
                 {"x": self.bid_max.x, "y": self.bid_max.y},
                 {"x": self.bid_min.x, "y": self.bid_min.y},
                 {"x": self.bid_min.x, "y": 0},
             ]
-        if self.ask_max:
-            response["ask_data"] = [
-                {"x": self.ask_max.x, "y": Y_MAX},
-                {"x": self.ask_max.x, "y": self.ask_max.y},
-                {"x": self.ask_min.x, "y": self.ask_min.y},
-                {"x": self.ask_min.x, "y": 0},
-            ]
-        return response
+        return [
+            {"x": self.ask_max.x, "y": Y_MAX},
+            {"x": self.ask_max.x, "y": self.ask_max.y},
+            {"x": self.ask_min.x, "y": self.ask_min.y},
+            {"x": self.ask_min.x, "y": 0},
+        ]
 
     def add_order(self, order: FloOrder):
-        self.send_update_to_frontend = True
-
-        is_buy = order.direction == "buy"
-        if is_buy:
-            self.bid_active = False
+        if order.direction == "buy":
             self.bid_max = FloPoint(order.max_price_point.x, order.max_price_point.y)
             self.bid_min = FloPoint(order.min_price_point.x, order.min_price_point.y)
-            self.buy_order_id = order.order_id
         else:
-            self.ask_active = False
             self.ask_max = order.max_price_point
             self.ask_min = order.min_price_point
-            self.sell_order_id = order.order_id
-        self.sort_points(adjust_buy=not is_buy)
+        BaseOrderGraph.add_order(self, order)
 
     def remove_order(self, order: FloOrder):
-        self.send_update_to_frontend = True
-
-        is_buy = order.direction == "buy"
-        self.reset_points(is_buy)
+        if order.direction == "buy":
+            self.bid_max = FloPoint(0, 1)
+            self.bid_min = FloPoint(1, 0)
+            if not self.ask_max:
+                self.ask_max = FloPoint(1, 20)
+                self.ask_min = FloPoint(0, 19)
+        else:
+            self.ask_max = FloPoint(1, 20)
+            self.ask_min = FloPoint(0, 19)
+            if not self.bid_max:
+                self.bid_max = FloPoint(0, 1)
+                self.bid_min = FloPoint(1, 0)
+        BaseOrderGraph.remove_order(self, order)
 
     def sort_points(self, adjust_buy):
         if self.ask_min.y <= self.bid_max.y:
@@ -74,37 +64,13 @@ class FloOrderGraph:
                     if self.bid_min.y >= self.bid_max.y:
                         self.bid_min.y = self.bid_max.y - 1
                 else:
-                    self.remove_points(remove_buy=True)
+                    self.bid_active = False
+                    self.bid_max, self.bid_min = None, None
             else:
                 if self.bid_max.y <= Y_MAX - 2:
                     self.ask_min.y = self.bid_max.y + 1
                     if self.ask_max.y <= self.ask_min.y:
                         self.ask_max.y = self.ask_min.y + 1
                 else:
-                    self.remove_points(remove_buy=False)
-
-    def remove_points(self, remove_buy):
-        if remove_buy:
-            self.bid_active = True
-            self.bid_max, self.bid_min = None, None
-        else:
-            self.ask_active = True
-            self.ask_max, self.ask_min = None, None
-
-    def reset_points(self, is_buy):
-        if is_buy:
-            self.bid_active = True
-            self.bid_max = FloPoint(0, 1)
-            self.bid_min = FloPoint(1, 0)
-            if not self.ask_max:
-                self.ask_active = True
-                self.ask_max = FloPoint(1, 20)
-                self.ask_min = FloPoint(0, 19)
-        else:
-            self.ask_active = True
-            self.ask_max = FloPoint(1, 20)
-            self.ask_min = FloPoint(0, 19)
-            if not self.bid_max:
-                self.bid_active = True
-                self.bid_max = FloPoint(0, 1)
-                self.bid_min = FloPoint(1, 0)
+                    self.ask_active = False
+                    self.ask_max, self.ask_min = None, None
