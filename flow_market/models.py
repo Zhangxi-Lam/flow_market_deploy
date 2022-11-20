c2757b18e343a3c6731043b1fa6dd7b4b07749bf
Zhangxi-Lam <jayce.linzx@gmail.com>
2022-11-19 17:09:36 -0800

    update quiz

--- flow_market/models.py
+++ flow_market/models.py
@@ -27,18 +27,22 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     buyer_quiz1 = models.StringField(
         label="Suppose you want to buy 400 units in 100 seconds. Suppose you set up your low price, high price, and max rate so that you are executing your buy order at a rate of 2 units per second. If nothing else changes in the market, are you going to acquire the 400 units that you want?",
+        widget=widgets.RadioSelect,
         choices=["Yes", "No"],
     )
     buyer_quiz2 = models.StringField(
         label="If you send a buy order with a high price of $15 per unit and a max rate of 5 units per second. If the current price is $16 per unit, what is the rate at which you would execute your order?",
+        widget=widgets.RadioSelect,
         choices=["0 unit per second", "5 units per second"],
     )
     buyer_quiz3 = models.StringField(
         label="Suppose you send a buy order with a low price of $10 per unit and a max rate of 5 units per second. If the current price is $9 per unit, what is the rate at which you would execute your order?",
+        widget=widgets.RadioSelect,
         choices=["0 unit per second", "5 units per second"],
     )
     buyer_quiz4 = models.StringField(
         label="Let us say you have bought 300 units up to the second 110, and you paid, on average, $10 per unit. Suppose you do not have any other transaction in the trading round. What is your profit for the round?",
+        widget=widgets.RadioSelect,
         choices=[
             "(12 - 10) * 500 = 1000",
             "(12 - 10) * 300 = 600",
@@ -48,6 +52,7 @@ class Player(BasePlayer):
     )
     buyer_quiz5 = models.StringField(
         label="Suppose you bought 500 units up to the second 110, and you paid, on average, $10 per unit. Suppose you do not have any other transaction in the trading round. What is your profit for the round?",
+        widget=widgets.RadioSelect,
         choices=[
             "(12 - 10) * 500 = 1000",
             "(12 - 10) * 300 = 600",
@@ -57,6 +62,7 @@ class Player(BasePlayer):
     )
     buyer_quiz6 = models.StringField(
         label="Suppose you bought 600 units up to the second 110, and you paid, on average, $10 per unit. Suppose you do not have any other transaction in the trading round. What is your profit for the round?",
+        widget=widgets.RadioSelect,
         choices=[
             "(12 - 10) * 600 + (0 - 10) * 100 = 200",
             "(12 - 10) * 500 + (0 - 10) * 100 = 0",
@@ -66,18 +72,22 @@ class Player(BasePlayer):
     )
     seller_quiz1 = models.StringField(
         label="Suppose you want to sell 600 units in 100 seconds. Suppose you set up your low price, high price, and max rate so that you are executing your sell order at a rate of 6 units per second. If nothing else changes in the market, are you going to sell the 600 units that you want?",
+        widget=widgets.RadioSelect,
         choices=["Yes", "No"],
     )
     seller_quiz2 = models.StringField(
         label="If you send a sell order with a low price equal to $10 per unit and a max rate of 6 units per second. If the current price is $8 per unit, what is the rate at which you would execute your order?",
+        widget=widgets.RadioSelect,
         choices=["0 unit per second", "6 units per second"],
     )
     seller_quiz3 = models.StringField(
         label="Suppose you send a sell order with a high price of $14 per unit and a max rate of 6 units per second. If the current price is $17 per unit, what is the rate at which you would execute your order?",
+        widget=widgets.RadioSelect,
         choices=["0 unit per second", "6 units per second"],
     )
     seller_quiz4 = models.StringField(
         label="Let us say you have sold 300 units up to the second 110 at an average price of $10 per unit. Suppose you do not have any other transaction in the trading round. What is your profit for the round?",
+        widget=widgets.RadioSelect,
         choices=[
             "(10 - 8) * 500 = 1000",
             "(10 - 8) * 300 = 600",
@@ -87,6 +97,7 @@ class Player(BasePlayer):
     )
     seller_quiz5 = models.StringField(
         label="Suppose you have sold 500 units up to the second 110 at an average price of $10 per unit. Suppose you do not have any other transaction in the trading round. What is your profit for the round?",
+        widget=widgets.RadioSelect,
         choices=[
             "(10 - 8) * 500 = 1000",
             "(10 - 8) * 300 = 600",
@@ -96,6 +107,7 @@ class Player(BasePlayer):
     )
     seller_quiz6 = models.StringField(
         label="Suppose you have sold 600 units up to the second 110 at an average price of $10 per unit. Suppose you do not have any other transaction in the trading round. What is your profit for the round?",
+        widget=widgets.RadioSelect,
         choices=[
             "(10 - 8) * 600 + (10 - 20) * 100 = 200",
             "(10 - 8) * 500 + (10 - 20) * 100 = 0",
